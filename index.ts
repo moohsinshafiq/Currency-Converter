4d04f91c3d1e619656dd64735579d9dd0abeb0b4
Mohsin Shafiq <145492941+moohsinshafiq@users.noreply.github.com>
2023-11-05 17:31:20 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,53 @@
+import inquirer from "inquirer"
+
+let Convertion = {
+    "PKR": {
+      "USD": 10,
+      "GBP": 5,
+      "PKR": 1
+    },
+    "GBP": {
+      "USD": 50,
+      "PKR": 150,
+      "GBP": 1
+    },
+    "USD": {
+      "PKR": 100,
+      "GBP": 50,
+      "USD": 1
+    }
+  }
+
+  const answer: {
+    from: "PKR"| "USD" | "GBP",
+    to: "PKR"| "USD" | "GBP",
+    amount: number
+  } = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select your currency: "
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select your convertion currency: "
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your convertion amount: "
+    }
+  ]);
+
+  const {from, to, amount} = answer;
+
+  if(from && to && amount) {
+    let result = Convertion[from][to] * amount;
+    console.log(`Your convertion from ${from} to ${to} is ${result}`)
+  } else {
+    console.log("Invalid inputs")
+  }
+
